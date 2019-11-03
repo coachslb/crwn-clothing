8cad94c76f095614bc0a82a4f9b2465d0c65b6ca
José Garção <josegarcao1@gmail.com>
2019-11-03 14:35:42 +0000

    add collection preview and collection item component logic and styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/Homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/Shop';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
